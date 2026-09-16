82b532419f386b13382c69dcd3d88104168bd5b1
Aryan Hapaliya <hapaliyaaryan24@gmail.com>
2026-09-16 19:20:00 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,10 +2,16 @@ let scrollContainer = document.querySelector('.gallery');
 let back = document.querySelector('#backbtn');
 let next = document.querySelector('#nextbtn');
 
-scrollContainer.addEventListener('wheek', (e) => {
+scrollContainer.addEventListener('wheel', (e) => {
     e.preventDefault();
     scrollContainer.scrollLeft += e.deltaY;
+    scrollContainer.style.scrollBehaviour = "auto";
 })
-next.addEventListener('click', (e) => {
-    e.preventDefault();
+next.addEventListener('click', () => {
+    scrollContainer.style.scrollBehavior = "smooth";
+    scrollContainer.scrollLeft += 900;
+})
+back.addEventListener('click', () => {
+    scrollContainer.style.scrollBehavior = "smooth";
+    scrollContainer.scrollLeft -= 900;
 })
